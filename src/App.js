1a00b50715915db4826db2052ceae0509e078ace
Mahmoud Ibrahim <mhmoud.ibrahim74@gmail.com>
2023-12-30 10:02:08 +0200

    useState

--- src/App.js
+++ src/App.js
@@ -3,25 +3,39 @@ import './App.css';
 
 import SearchBar from "./components/SearchBar";
 import Header from "./components/Header";
-import { BrowserRouter,Routes,Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import NotFound from "./components/NotFound";
 import Home from "./components/Home";
+import { useState } from "react";
+
 function App() {
+  const [count, setCount] = useState(1);
+
+  const handleIncrement = () => {
+    setCount(count + 1);
+  };
+
+  const handleDecrement = () => {
+    setCount(count - 1);
+  };
 
   return (
     <div className="App">
+      <p>{count}</p>
+      <button onClick={handleIncrement}>+</button>
+      <button onClick={handleDecrement}>-</button>
+      
       <>
-      <BrowserRouter>
-       <Routes>
-            <Route path="/header" element={<Header/>} />
+        <BrowserRouter>
+          <Routes>
+            <Route path="/header" element={<Header />} />
             <Route index element={<Home />} /> {/* return to default page index */}
-            <Route path="*" element={<NotFound></NotFound>}/>   {/*if  path doesn't exsit return NotFound */}
-       </Routes>
-      </BrowserRouter>
+            <Route path="*" element={<NotFound />}/> {/* if path doesn't exist return NotFound */}
+          </Routes>
+        </BrowserRouter>
 
         <SearchBar></SearchBar>
       </>
-
     </div>
   );
 }
