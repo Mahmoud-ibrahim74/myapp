629a9ed87de723c52547a24bb35243527564da7f
Mahmoud Ibrahim <mhmoud.ibrahim74@gmail.com>
2023-12-30 10:58:41 +0200

    useContext

--- src/App.js
+++ src/App.js
@@ -6,7 +6,8 @@ import Header from "./components/Header";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import NotFound from "./components/NotFound";
 import Home from "./components/Home";
-import { useEffect, useRef, useState } from "react";
+import { createContext, useEffect, useRef, useState } from "react";
+export const alertMe = createContext();
 
 function App() {
   const [count, setCount] = useState(1);
@@ -18,21 +19,31 @@ function App() {
   const handleDecrement = () => {
     setCount(count - 1);
   };
+
+// Start useEffect
  useEffect(()=>{
   console.log("Test");
  },[count]) // in this case of useEffect print Test only one ,
   // With One Render,if an array is empty
   // , if not array can take state and this case will re-render only state
 
-//  useEffect(()=>{
-//   console.log("Test");
-//  }); // in this case of useEffect print Test with each render on page 
- const focus = useRef(null);
+ useEffect(()=>{
+  console.log("Test");
+ }); // in this case of useEffect print Test with each render on page 
+// End  useEffect
+
+
+// End useRef
+ const focus = useRef(null); // like a pointer to element and can control in this element 
  useEffect(()=> {
   focus.current.focus();
  },[focus])
+ // End useRef
+
+ 
   return (
     <div className="App">
+      <alertMe.Provider value="alert Me"></alertMe.Provider>
       <p>{count}</p>
       <button onMouseDown={handleIncrement}>+</button>
       <button onMouseDown={handleDecrement}>-</button>
--- src/components/NotFound.js
+++ src/components/NotFound.js
@@ -1,21 +1,24 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { Link } from 'react-router-dom';
 import './NotFound.css';
+import { alertMe } from '../App';
 const NotFound = () => {
+  const alertMessage = useContext(alertMe);
   return (
+
     <div id='oopss'>
-    <div id='error-text'>
-        <img src="https://cdn.rawgit.com/ahmedhosna95/upload/1731955f/sad404.svg" alt="404"/>
-        <span>404 PAGE</span>
+       <div id='error-text'>
+        <img src="https://cdn.rawgit.com/ahmedhosna95/upload/1731955f/sad404.svg" alt="404" />
+        <span>{alertMessage}</span>
         <p class="p-a">
-           . The page you were looking for could not be found</p>
+          . The page you were looking for could not be found</p>
         <p class="p-b">
-            ... Back to previous page
+          ... Back to previous page
         </p>
         <a href="/" class="back">... Back to previous page</a>
         <Link to="/"></Link>  {/* this component at the same <a> above*/}
+      </div>
     </div>
-</div>
   )
 }
 
